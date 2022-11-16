fa4a06ce0f66d44900571e06951f9784cc041d30
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-11-16 16:26:23 -0300

    ejercicio 2 de JS parte 1 listo

--- U2:M3/script.js
+++ U2:M3/script.js
@@ -1,13 +1,9 @@
+// EJERCICIO 1
 // Crear el código que dada una distancia determine el medio de transporte apropiado de acuerdo a las siguientes reglas:
-
 // 0 a 1000 metros = pie
-
 // 1000 a 10000 metros = bicicleta
-
 // 10000 a 30000 metros = colectivo
-
 // 30000 a 100000 metros = auto
-
 // +100000 = avion
 
 let distancia = parseInt(prompt("Indiquenos cuántos mt tiene que recorrer para recomendarle el medio de transporte"));
@@ -31,5 +27,19 @@ if (distancia <= 1000) {
     } else {
         console.log(`Si la distancia es ${distancia}, le recomendamos ir en avión`);
         document.write(`Si la distancia es ${distancia}, le recomendamos ir en avión`)
-
 }
+
+// EJERCICIO 2
+// Crear el código que reciba como parámetro un array de números y devuelva el mayor.
+
+const numeros = [0, 34, 209, 83, 500, 9];
+let mayor = 0;
+
+for (let i = 0; i<numeros.length; i++) {
+    console.log("Entra al for");
+    if (numeros[i] > mayor) {
+        mayor = numeros[i];
+    }
+}
+console.log(`El mayor de ${numeros} es ${mayor}`);
+
