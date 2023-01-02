8f9327d79e5165b10398dccb58a993fea6123f5c
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-01-02 20:47:16 -0300

    sesiones con Express en correcto funcionamiento

--- M4:U4/app.js
+++ M4:U4/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
+// ROUTERS
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,8 +22,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// aca coloco el codigo de express-session, ANTES de los routers, sino NO va a funcionar.
+app.use(session({
+  secret: "agustinafernandez2022",
+  resave: false,
+  saveUninitialized: true
+}));
+
+// usando los ROUTERS
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  let conocido = Boolean(req.session.usuario);
+
+  res.render('index', {
+    title: 'Tarea sesiones en Express',
+    conocido: conocido,
+    usuario: req.session.usuario
+  });
+});
+
+app.post('/ingresar', (req, res) => {
+  if (req.body.usuario) {
+    req.session.usuario = req.body.usuario
+  }
+  res.redirect('/')
+});
+
+app.get('/salir', (req, res) => {
+  req.session.destroy();
+  res.redirect('/')
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
