860a6596614e637b71f665e4c749ed640e3b447c
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-01-02 19:13:12 -0300

    ahora SI funcionan los routers (controladores). Estaba repitiendo el path por eso no funcionaba

--- M4:U3/routes/contactanos.js
+++ M4:U3/routes/contactanos.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET contactanos page. */
-router.get('/contactanos', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('contactanos'); /*contactanos.hbs*/
 });
 
--- M4:U3/routes/galeria.js
+++ M4:U3/routes/galeria.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET galeria page. */
-router.get('/galeria', function(req, res, next) {
-  res.send('galeria'); /*galeria.hbs*/
+router.get('/', function(req, res, next) {
+  res.render('galeria'); /*galeria.hbs*/
 });
 
 module.exports = router;
