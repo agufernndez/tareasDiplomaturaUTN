bf1be461aa8af3b791fee07430f60ae63a9cdce6
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-01-06 08:40:08 -0300

    proyecto con express, descargue dependencias (dotenv, util, mysql), cree js en carpeta models)

---
+++ M5:U3/models/bd.js
@@ -0,0 +1,21 @@
+let mysql = require('mysql');
+let util = require('util');
+
+var pool = mysql.createPool({
+    connectionLimit:10,
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DB_NAME,
+});
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
+
+
+
+// MYSQL_HOST=localhost
+// MYSQL_DB_NAME=tareaM5:U2
+// MYSQL_USER=root
+// MYSQL_PASSWORD=root
