baea6961f48ea0e938182a5a389ff00a18fc640e
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2023-01-29 11:24:29 -0300

    agregue insert, update, delete

--- M5:U3/app.js
+++ M5:U3/app.js
@@ -27,14 +27,45 @@ app.use('/users', usersRouter);
 
 // CONSULTAS a la base de datos
 // SELECT
-pool.query('select nombre, apellido from empleados').then(function (resultados) {
-  console.log(resultados);
-});
+// pool.query('select * from empleados').then(function (resultados) {
+//   console.log(resultados);
+// });
 
-// UPDATE
+// INSERT / en la consola va a aparecer que lo agrego PERO no aparece toda la lista de empleados, sino que va a aparecer en la base de datos de php admin
+// let obj = {
+//   nombre: 'Juan Ignacio',
+//   apellido: 'Garaygorta',
+//   trabajo: 'kinesiologo',
+//   edad: '27',
+//   salario: 2000,
+//   mail: 'juangaray0@gmail.com'
+// }
+
+// pool.query('insert into empleados set ?', [obj]).then(function (resultados) {
+
+//   console.log(resultados)
+// });
+
+// UPDATE / idem anterior, el cambio se ve en la base de datos. en la consola dice que se coincidio el id, se cambio blabla
+// let id = 24;
+// let obj2 = {
+//   nombre: 'Agustina',
+//   apellido: 'Fernandez',
+//   trabajo: 'Programadora',
+//   salario: 1800,
+//   mail: 'lic.fernandezagustina@gmail.com'
+// }
+
+// pool.query('update empleados set ? where id_emp=?', [obj2, id]).then(function(resultados) {
+//   console.log(resultados)
+// });
 
 // DELETE
-
+// let id = 25 lo borre, lo comente y puse el 26 para tambien borrarlo porque queria borrar 25 y 26
+let id = 26;
+pool.query('delete from empleados where id_emp=?', id).then(function (resultados) {
+  console.log(resultados)
+})
 
 
 
